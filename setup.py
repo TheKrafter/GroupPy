ce96750ceee689cfd9e8e062d133857cfe833e21
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-03-13 16:41:16 -0500

    initial layout of library files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+setup(
+    name='grouppy',
+    packages=find_packages(),
+    version='0.0.1',
+    description='A Python Library for interacting with the GroupMe Rest API ',
+    author='TheKrafter',
+    license='GPL-3.0',
+    install_requires=['rest3client'], # https://github.com/soda480/rest3client
+    setup_requires=['setuptools', 'pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+)
