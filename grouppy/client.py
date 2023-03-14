43707dc44f99b186174c398c4a8b17f65dd0ff10
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-03-14 07:53:22 -0500

    adding webserver for oauth

--- grouppy/client.py
+++ grouppy/client.py
@@ -1,5 +1,6 @@
 from rest3client import RESTclient # For API Requests
 import webbrowser # For opening browser during OAuth
+from flask import Flask, request
 
 class GroupMeClient():
     def __init__(client_id):
@@ -10,7 +11,13 @@ class GroupMeClient():
     def authenticate(self):
         """ Authenticate with GroupMe """
         webbrowser.open(f'https://oauth.groupme.com/oauth/authorize?client_id={self.client_id}')
-        # Start Webserver at localhost
+
+        @app.route('/oauth', methods=['GET'])
+        def search():
+            args = request.args
+            name = args.get('access_token')
+            
+        
         # Store URL
         # Set new self.api with new token
         # Save token as variable
@@ -19,5 +26,8 @@ class GroupMeClient():
     def get_groups(self):
         """ Fetch Groups from the API """
         response = self.api.get('/groups', per_page='200')
-        self
+        self.groups = response['response']
+        self.group_ids = []
+        for itm in self.groups:
+            self.group_ids.append(self.groups[itm]['id'])
         
