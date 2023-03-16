95d18144fd763f1e921b428b14baf706fcca36c9
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-03-16 07:27:27 -0500

    load client_id from file

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,9 @@
 from grouppy import GroupMeClient
 
-token = input('\nGroupMe Client ID: ')
+with open('client_id.txt', 'r') as client_txt:
+    token = client_txt
+
+print(f'\nGroupMe Client ID retrieved from file.')
 
 client  = GroupMeClient(token, oauth_wait_time=30)
 
