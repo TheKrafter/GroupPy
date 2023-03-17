44013524db382f4f7ff965fa550afc008d504586
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-03-17 11:17:43 -0500

    fix requires

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='A Python Library for interacting with the GroupMe Rest API ',
     author='TheKrafter',
     license='GPL-3.0',
-    install_requires=['rest3client', 'flask'], # https://github.com/soda480/rest3client
+    install_requires=['requests', 'flask', 'guli'],
     setup_requires=['setuptools', 'pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
