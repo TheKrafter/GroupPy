02cd74299f8ab7b3ca696a21ddf39cf37f7afb3d
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-04-18 07:58:34 -0500

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='grouppy',
     packages=find_packages(),
-    version='0.0.1',
+    version='0.1.0',
     description='A Python Library for interacting with the GroupMe Rest API ',
     author='TheKrafter',
     license='GPL-3.0',
