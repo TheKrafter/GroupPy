b258e9fa915a264e819b729db718397a705569e9
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-04-18 21:38:13 -0500

    not testing listener yet, test message fetching

--- tests/test_client_2.py
+++ tests/test_client_2.py
@@ -1,6 +1,6 @@
 import datetime
 
-from grouppy import GroupMeClient, GroupMeListener
+from grouppy import GroupMeClient
 
 print(f'Welcome to GroupPy Manual Testing!')
 
@@ -10,8 +10,10 @@ print(f'\nFetching Groups...')
 for group in client.get_groups():
     print(f'> {group["name"]}\n  {group["group_id"]}')
 
-print(f'\nTesting message sending...')
+print(f'\nTesting message sending and fetching...')
 group = input(f'> Paste a Group ID to send a message in: ')
+for message in client.get_messages(group)["messages"]:
+    print(f'[{datetime.datetime.fromtimestamp(message["created_at"]).strftime("%Y-%m-%d %H:%M:%S")}] {message["name"]} : {message["text"]}')
 message = input(f'> Type your message: ')
 print(f'> Sending Message "{message}"...')
 
