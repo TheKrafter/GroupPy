57b735f9effe4a65b8713e91dc458617f1ce396a
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-04-20 21:21:21 -0500

    change libraries

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='A Python Library for interacting with the GroupMe Rest API ',
     author='TheKrafter',
     license='GPL-3.0',
-    install_requires=['requests', 'flask', 'guli', 'websocket-client'],
+    install_requires=['requests', 'flask', 'guli', 'websockets'],
     setup_requires=['setuptools', 'pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
